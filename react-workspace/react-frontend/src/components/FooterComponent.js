7f1723bb044db34d8989a1098c3918b61d09dfff
vijayalakshmi231 <vijayalakshmigoriparthi231@gmail.com>
2024-10-01 16:57:11 +0530

    react-frontend

---
+++ react-workspace/react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../App.css';
+
+	export default class FooterComponent extends Component {
+ 	 render() {
+    	return (
+      	<div>
+        	<footer className='footer'>
+       <span>All Right Reserved 2023 &copy;NIYAZ_SIR </span>
+        </footer>
+     	 </div>
+    	)
+  	}
+	}
---
+++ react-workspace/react-frontend/src/components/HeaderComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+	export default class HeaderComponent extends Component {
+  	render() {
+    	return (
+      	<div>
+            	
+                <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+                	    <div className='container'>
+          <a href="" className='navbar-brand '>Employee Management Application</a>
+                    	</div>
+                	</nav>
+         	  
+      	</div>
+    	)
+  	}
+	}
+
